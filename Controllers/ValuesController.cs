07f4b0d5145a43fa592461254fae327d39a92c0b
asharma72 <ag.sharma23@gmail.com>
2018-07-23 13:30:41 +0530

    second commit

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace mydotnetcoreapp.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        // GET api/values/hhh
         [HttpGet]
         public IEnumerable<string> Get()
         {
